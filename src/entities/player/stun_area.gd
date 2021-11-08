e56e2287e51be0ffc231ce5998e4f4ca6382b9b6
Austin Simmons <autto91@gmail.com>
2021-11-07 16:56:45 -0800

    added sprite effect for stun arm

--- src/entities/player/stun_area.gd
+++ src/entities/player/stun_area.gd
@@ -9,21 +9,29 @@ enum FireDirection {
 var is_firing := false
 
 onready var timer := $FireTimer
+onready var sprite := $Sprite
 
 
-func fire(offset = Vector2.ZERO):
+func fire(offset = Vector2.ZERO, direction = 0):
 	if not is_firing:
+		if direction == 1:
+			sprite.flip_h = true
+
 		is_firing = true
 		position += offset
+		sprite.visible = true
 		pause_mode = Node.PAUSE_MODE_PROCESS
 		timer.start()
 
 
 func _ready() -> void:
+	sprite.visible = false
 	pause_mode = Node.PAUSE_MODE_STOP
 
 
 func _on_timer_timeout() -> void:
 	is_firing = false
+	sprite.visible = false
+	sprite.flip_h = false
 	position = Vector2.ZERO
 	pause_mode = Node.PAUSE_MODE_STOP
